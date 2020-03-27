ffe1f1e5c6fe56844520f4bdf9cc53dd4f5541b9
Charles Buxton <chazbux@gmail.com>
2020-03-27 09:18:18 -0600

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,12 @@ Catergory.create(name: 'React General')
 Catergory.create(name: 'React Props')
 Catergory.create(name: 'React Hooks')
 Catergory.create(name: 'React Context')
-Catergory.create(name: 'React Frontend')
 
 
 # t.string "name"
 # t.integer "points"
 # t.boolean "flipped"
+# t.text "answer"
 Card.create(name: 'What is React?', points: 100, flipped: false, catergory_id: 1, answer: 'A Javascript frontend libaray that creates a single page application and manupilates the view via a virtual DOM.')
 Card.create(name: 'What are React Components?', points: 200, flipped: false, catergory_id: 1, answer: 'Components are a design that allow us to easily insert and reuse individual parts and pieces of code.' )
 Card.create(name: 'Who started React?', points: 300, flipped: false, catergory_id: 1, answer:'Facebook')
@@ -28,22 +28,16 @@ Card.create(name: 'How can you pass data to a parent function with props?', poin
 Card.create(name: 'How can we use props for styling?', points: 400, flipped: false, catergory_id: 2, answer: 'Pass some style props.')
 Card.create(name: 'How can you pass prop data through nested components and back up?', points: 500, flipped: false, catergory_id: 2, answer: 'Props>this.props>this.props')
 
-Card.create(name: '', points: 100, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 200, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 300, flipped: false, catergory_id: 2, answer:'' )
-Card.create(name: '', points: 400, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 500, flipped: false, catergory_id: 2, answer: '')
+Card.create(name: 'What are React Hooks?', points: 100, flipped: false, catergory_id: 2, answer: 'Functions that have special abilities to give our functional components more horsepower.')
+Card.create(name: 'What does the useContext hook do?', points: 200, flipped: false, catergory_id: 2, answer: 'Allows us to easily access state living within a provider')
+Card.create(name: 'How can we use state in a functional component?', points: 300, flipped: false, catergory_id: 2, answer:'useState()' )
+Card.create(name: 'How can we do something like a componentDidMount with Hooks?', points: 400, flipped: false, catergory_id: 2, answer: 'useEffect()')
+Card.create(name: 'What can we NOT use hooks on?', points: 500, flipped: false, catergory_id: 2, answer: 'Class Components')
 
-Card.create(name: '', points: 100, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 200, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 300, flipped: false, catergory_id: 2, answer:'' )
-Card.create(name: '', points: 400, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 500, flipped: false, catergory_id: 2, answer: '')
-
-Card.create(name: '', points: 100, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 200, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 300, flipped: false, catergory_id: 2, answer:'' )
-Card.create(name: '', points: 400, flipped: false, catergory_id: 2, answer: '')
-Card.create(name: '', points: 500, flipped: false, catergory_id: 2, answer: '')
+Card.create(name: 'What is React Context?', points: 100, flipped: false, catergory_id: 2, answer: 'A system to directly pass props to nested components easily.')
+Card.create(name: 'When should we use Context?', points: 200, flipped: false, catergory_id: 2, answer: 'When we want to share data that is considered "Gloabal" in applcation scope.')
+Card.create(name: 'What is one difficuly of using Context?', points: 300, flipped: false, catergory_id: 2, answer:'It makes reuse of components more difficult.' )
+Card.create(name: 'How do we use a context provider with a class component?', points: 400, flipped: false, catergory_id: 2, answer: 'Wrap the JSX component with provider, then pass data as props.')
+Card.create(name: 'What are the steps to making a provider?', points: 500, flipped: false, catergory_id: 2, answer: 'Create context variable via React.createContext()-- Create context provider and export -- Create context consumer to call in component. ')
 
 puts "Data Seeded."
